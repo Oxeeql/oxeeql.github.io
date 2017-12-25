f85162a49722d49d5147f07316b95715dd620f65
rooti <rooti@localhost.localdomain>
2017-12-26 00:34:49 +0100

    nav ausgelagert

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+
+<html>
+<head>
+    <title>Christian Stehle</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+</head>
+<?php
+include("nav.html");
+include("indexContent.html")
+?>
+<body class="homepage">
+<div id="page-wrapper">
+
+    <!-- Header -->
+    <div id="header">
+
+    </div>
+</div>
+</body>
+
+</html>
+
+
